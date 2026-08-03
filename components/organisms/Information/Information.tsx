8d5183695246a1403f480f29689f518a1c93bcf5
George Munday <geogeM1@icloud.com>
2026-08-04 00:43:09 +0100

    refactor: remove commented-out code in Information component for cleaner readability

--- components/organisms/Information/Information.tsx
+++ components/organisms/Information/Information.tsx
@@ -1,14 +1,5 @@
 const Information = () => {
   return (
-    // <div className="flex flex-col items-center justify-center gap-4  p-10 font-sans rounded-2xl dark:bg-black mb-10 ">
-    //   <h1 className="text-3xl font-bold">Information</h1>
-    //   <p className="text-center">
-    //     This is a Some random dashboard that i have built to show employers my skills
-    //   </p>
-    //   <p>and the fact that i can build full-stack applications with modern technologies.</p>
-    //   <p>Feel free to explore the dashboard and see what it has to offer.</p>
-    //   <p>Thank you for visiting!</p>
-    // </div>
     <div className="flex flex-col items-center justify-center gap-4 p-6 font-sans rounded-2xl ">
       <div className="rounded-xl bg-white p-6 shadow dark:bg-neutral-900 w-full">
             <h2 className="text-lg font-semibold text-blue-600">Information</h2>
